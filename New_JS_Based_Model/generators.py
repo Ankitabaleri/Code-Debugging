e61a11b7b666dfb03d495ceb36c7803ed346a975
Ankita <ankita@Mac-15436.lan>
2025-04-21 19:25:58 -0700

    done

--- New_JS_Based_Model/generators.py
+++
@@ -1,9 +0,0 @@
-# generators.py
-
-from model import StarCoder, ModelBase
-
-def model_factory(model_name: str, port: str = "") -> ModelBase:
-    if "starcoder" in model_name.lower():
-        return StarCoder(port)
-    else:
-        raise ValueError(f"Model {model_name} is not supported.")
---
+++ try.js
@@ -0,0 +1,6 @@
+function sum() {
+    return 5+4;
+}
+
+console.assert(sum() === 9)
+console.assert(sum() === 6)
